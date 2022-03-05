f9d9edcd7781933d0417b7e6a6769dc2c5781767
let1s3n <letisendev@gmail.com>
2022-03-05 04:50:00 -0500

    fix

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,18 @@
-import React from 'react'
-import { Container, ToastContainer, Toast, Image, ButtonGroup, Button,Row,Col } from 'react-bootstrap'
+import React,{useState} from 'react'
+import { Container, ToastContainer, Toast, Image, ButtonGroup, Button, Row, Col, ToggleButton } from 'react-bootstrap'
 import ppPlaceholder from '../assets/img/pp-placeholder.svg'
 import arrowDown from '../assets/img/arrow-down.svg'
 import routineIcon from '../assets/img/routine-icon.svg'
 import excerciseIcon from '../assets/img/excercise-icon.svg'
 import cronoIcon from '../assets/img/crono-icon.svg'
+import colDivider from '../assets/img/col-divider.svg'
 const Home = () => {
+  const [checked, setChecked] = useState(false);
+  const [radioValue, setRadioValue] = useState("1");
   return (
     <Container className="g-0 vh-100 d-flex flex-column pt-13 align-items-center">
       <ToastContainer className="p-3" position="top-end">
         <Toast>
-          {/* <Toast.Header closeButton={false}>
-              <img
-                src="holder.js/20x20?text=%20"
-                className="rounded me-2"
-                alt=""
-              />
-              <strong className="me-auto">Bootstrap</strong>
-              <small>11 mins ago</small>
-            </Toast.Header> */}
           <Toast.Body className="d-flex py-1 px-2 justify-content-between">
             <div className="d-flex" style={{ columnGap: .5 + 'rem' }}>
               <Image src={ppPlaceholder} />
@@ -36,25 +30,66 @@ const Home = () => {
       </h4>
       <p className="m-0 custom-font-1 pb-5 text-secondary">Tus métricas generales son las siguientes:</p>
       <ButtonGroup className="pb-5" style={{ columnGap: 18 + 'px' }}>
-        <Button variant="outline-primary" className="rounded-pill px-6" style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}>Año</Button>
+        {/* <Button variant="outline-primary" className="rounded-pill px-6" style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}>Año</Button>
         <Button variant="outline-primary" className="rounded-pill px-6" style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}>Mes</Button>
-        <Button variant="outline-primary" className="rounded-pill px-6" style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}>Semana</Button>
+        <Button variant="outline-primary" className="rounded-pill px-6" style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}>Semana</Button> */}
+        <ToggleButton
+          id="btnradio1"
+          type="radio"
+          variant="outline-primary"
+          name="radio"
+          className="rounded-pill px-6"
+          value="1"
+          checked={radioValue === "1"}
+          onChange={(e) => setRadioValue(e.currentTarget.value)}
+          style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}
+        >
+          Año
+        </ToggleButton>
+        <ToggleButton
+          id="btnradio2"
+          type="radio"
+          variant="outline-primary"
+          name="radio"
+          className="rounded-pill px-6"
+          value="2"
+          checked={radioValue === "2"}
+          onChange={(e) => setRadioValue(e.currentTarget.value)}
+          style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}
+        >
+          Mes
+        </ToggleButton>
+        <ToggleButton
+          id="btnradio3"
+          type="radio"
+          variant="outline-primary"
+          name="radio"
+          className="rounded-pill px-6"
+          value="3"
+          checked={radioValue === "3"}
+          onChange={(e) => setRadioValue(e.currentTarget.value)}
+          style={{ paddingTop: 6 + 'px', paddingBottom: 6 + 'px' }}
+        >
+          Semana
+        </ToggleButton>
       </ButtonGroup>
       <div className="mini-dashboard d-flex">
         <Row className="g-0 w-100 font-secondary" xs={3}>
-          <Col className="d-flex flex-column align-items-center py-5 px-7">
+          <Col className="d-flex flex-column align-items-center py-5 px-7 position-relative">
             <Image className="mb-3" src={routineIcon} style={{ width: 2 + 'rem' }} />
-            <h1 className="mb-2 fw-bold text-primary">25</h1>
+            <h1 className="m-0 fw-bold text-primary">25</h1>
             <p className="m-0 font-sm font-primary">Rutinas realizadas</p>
+            <Image src={colDivider} className="position-absolute end-0 bottom-0 pb-5"/>
           </Col>
-          <Col className="d-flex flex-column align-items-center py-5 px-7">
+          <Col className="d-flex flex-column align-items-center py-5 px-7 position-relative">
             <Image className="mb-3" src={excerciseIcon} style={{ width: 2 + 'rem' }} />
-            <h1 className="mb-2 fw-bold text-primary">124</h1>
+            <h1 className="m-0 fw-bold text-primary">124</h1>
             <p className="m-0 font-sm font-primary">Ejercicios realizados</p>
+            <Image src={colDivider} className="position-absolute end-0 bottom-0 pb-5"/>
           </Col>
           <Col className="d-flex flex-column align-items-center py-5 px-7">
             <Image className="mb-3" src={cronoIcon} style={{ width: 2 + 'rem' }} />
-            <h1 className="mb-2 fw-bold text-primary">146 <span className="fs-5">min.</span> </h1>
+            <h1 className="m-0 fw-bold text-primary">146 <span className="fs-5">min.</span> </h1>
             <p className="m-0 font-sm text-black font-primary">Tiempo de ejercicio</p>
           </Col>
         </Row>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,8 +1,77 @@
 import React from 'react'
-
+import { Container, Row, Col, Image, Form, Button } from 'react-bootstrap'
+import ppPlaceholderBig from '../assets/img/pp-placeholder-big.svg'
 const Profile = () => {
   return (
-    <h1>Profile</h1>
+    <Container className="g-0 vh-100 d-flex flex-column pt-13 align-items-center">
+      <div>
+        <Row className="g-0 mb-2">
+          <h5 className="m-0 text-primary">Mi cuenta</h5>
+        </Row>
+        <Row className="g-0" style={{ columnGap: 1 + 'rem' }} xs="auto">
+          <Col className="profile-col d-flex flex-column align-items-center justify-content-center" style={{ width: 222 + 'px', height: 291 + 'px' }}>
+            <Image src={ppPlaceholderBig} className="mb-5" />
+            <div>
+              <p className="mb-2 font-sm text-primary fw-bold text-decoration-underline">Subir una foto nueva</p>
+              <p className="m-0 text-primary font-sm text-decoration-underline">Quitar foto</p>
+            </div>
+          </Col>
+          <Col className="profile-col p-3 position-relative" style={{ width: 399 + 'px', height: 474 + 'px' }}>
+            <Form>
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicNombre" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Nombre</Form.Label>
+                <Form.Control type="text" placeholder="Ingrese su nombre" className="py-2 px-3 font-sm text-secondary" />
+                {/* <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+                </Form.Text> */}
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicApellido" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Apellido</Form.Label>
+                <Form.Control type="text" placeholder="Ingrese su apellido" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicEmail" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Correo Electrónico</Form.Label>
+                <Form.Control type="email" placeholder="rufian.contreri@company.com" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicEmpresa" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Organización</Form.Label>
+                <Form.Control type="text" placeholder="My Company" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicNacimiento" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Fecha de nacimiento</Form.Label>
+                <Form.Control type="date" placeholder="dd/mm/yyyy" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicPeso" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Peso</Form.Label>
+                <Form.Control type="text" placeholder="kg" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicTalla" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Talla</Form.Label>
+                <Form.Control type="text" placeholder="cm" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+
+              <Form.Group className="mb-1 d-flex align-items-center" controlId="formBasicContraseña" style={{ columnGap: 2 + 'rem' }}>
+                <Form.Label className="m-0 font-sm text-secondary">Contraseña</Form.Label>
+                <Form.Control type="password" placeholder="********" className="py-2 px-3 font-sm text-secondary" />
+              </Form.Group>
+              <div className="d-flex m-0 justify-content-end px-2">
+                <p className="m-0 font-sm text-primary text-decoration-underline">Cambiar contraseña</p>
+              </div>
+              <Button variant="primary" type="submit" className="position-absolute btn-submit">
+                Guardar
+              </Button>
+            </Form>
+
+          </Col>
+        </Row>
+      </div>
+    </Container>
   )
 }
 
